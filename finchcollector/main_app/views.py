58ea945bcb634766dc7715dbb86603da9b791b3c
Ibrahim <ibrahimalhanich@gmail.com>
2020-06-29 14:37:06 -0300

    Details page added for Finches

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,30 +1,35 @@
 from django.shortcuts import render
-
+from .models import Finch
 # Add the following import
 from django.http import HttpResponse
 
 
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+# class Finch:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
 
-finches = [
-  Finch('Lolo', 'tabby', 'foul little demon', 3),
-  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Finch('Raven', 'black tripod', '3 legged cat', 4)
-]
+# finches = [
+#   Finch('Lolo', 'tabby', 'foul little demon', 3),
+#   Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Finch('Raven', 'black tripod', '3 legged cat', 4)
+# ]
 
 
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Finch Collector!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html') 
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
