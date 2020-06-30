debed3851e0d012a0b1a858c3eb69d2b3aabfdb4
Ibrahim <ibrahimalhanich@gmail.com>
2020-06-30 13:24:22 -0300

    Feeding Forms Added & Details Page Updated

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Finch
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
 # Add the following import
 # from django.http import HttpResponse
 
@@ -45,4 +46,15 @@ class FinchDelete(DeleteView):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {
+     'finch': finch, 'feeding_form': feeding_form 
+  })
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)  
