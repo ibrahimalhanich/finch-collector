b91eeb4874eb3dfbe7accbd373e5b1215248491e
Ibrahim <ibrahimalhanich@gmail.com>
2020-06-30 15:36:47 -0300

    Add toys and established relationship between many toys and many finches

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Finch
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 # Add the following import
 # from django.http import HttpResponse
@@ -47,8 +48,10 @@ class FinchDelete(DeleteView):
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   feeding_form = FeedingForm()
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
   return render(request, 'finches/detail.html', {
-     'finch': finch, 'feeding_form': feeding_form 
+     'finch': finch, 'feeding_form': feeding_form,
+     'toys': toys_finch_doesnt_have
   })
 
 def add_feeding(request, finch_id):
@@ -58,3 +61,27 @@ def add_feeding(request, finch_id):
     new_feeding.finch_id = finch_id
     new_feeding.save()
   return redirect('detail', finch_id=finch_id)  
+
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+def assoc_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
